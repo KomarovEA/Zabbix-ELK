4148658f6b1cf6e3791d861b6b8201565961a064
KomarovEA <55788385+KomarovEA@users.noreply.github.com>
2019-10-20 22:44:20 +0300

    End of task2- 1.2
    End of task2- 1.2

---
+++ Task2/1.2/zabag-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Installing and configuring Zabbix Agent on CentOS
+
+#yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+yum -y install http://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-release-4.2-2.el7.noarch.rpm
+yum -y install zabbix-agent
+cat <<EOFZAD >> /etc/zabbix/zabbix_agentd.conf
+# Common
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+LogFile=/var/log/zabbix/zabbix_agentd.log
+DebugLevel=3
+# Passive checks related
+Server=$1
+Hostname=$2
+ListenPort=10050
+ListenIP=0.0.0.0
+StartAgents=3
+# Active checks related
+ServerActive=$1
+# no this settings!!! ServerPort=10051
+Hostname=$2
+EOFZAD
+systemctl restart zabbix-agent
+systemctl enable zabbix-agent
+# Zabbix sender installing
+yum install -y zabbix-sender
+
---
+++ Task2/1.2/zabsrv-config.4.2.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Installing and configuring MySQL DB (MariaDB)
+yum -y install mariadb mariadb-server
+
+#  Mysql Initial configuration (before initial start SQL!!!)
+mysql_install_db --user=mysql --force
+systemctl restart mariadb
+systemctl enable mariadb
+mysqladmin -u root password ''
+mysql -uroot -e "create database IF NOT EXISTS $3 character set utf8 collate utf8_bin";
+mysql -uroot -e "grant all privileges on $3.* to $3@localhost identified by '$4'";
+
+# Installing and configuring Zabbix Server
+
+#yum -y install http://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
+yum -y install http://repo.zabbix.com/zabbix/4.2/rhel/7/x86_64/zabbix-release-4.2-2.el7.noarch.rpm
+yum -y install zabbix-server-mysql
+yum -y install zabbix-server-mysql
+yum -y install zabbix-server-mysql
+yum -y install zabbix-server-mysql
+yum -y install zabbix-server-mysql
+yum -y install zabbix-server-mysql
+
+#Import initial schema, data and configuring
+zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql --database=$2 --user=$3 --password=$4;
+#zcat /usr/share/doc/zabbix-server-mysql-3.2.11/create.sql.gz | mysql --database=$2 --user=$3 --password=$4;
+#zcat /usr/share/doc/zabbix-server-mysql-4.4/create.sql.gz | mysql --database=$2 --user=$3 --password=$4;
+#zcat /usr/share/doc/zabbix-server-mysql-4.2.7/create.sql.gz | mysql --database=$2 --user=$3 --password=$4;
+cat <<EOFZCDF >> /etc/zabbix/zabbix_server.conf 
+DBHost=localhost
+DBName=$2
+DBUser=$3
+DBPassword=$4
+
+EOFZCDF
+systemctl enable zabbix-server
+systemctl restart zabbix-server
+
+# Zabbix front-end Installation and Configuration
+yum -y install zabbix-web-mysql
+yum -y install zabbix-web-mysql
+yum -y install zabbix-web-mysql
+yum -y install zabbix-web-mysql
+yum -y install zabbix-web-mysql
+
+cat <<EOFZCF > /etc/httpd/conf.d/zabbix.conf
+#
+# Zabbix monitoring system php web frontend
+#
+DocumentRoot /usr/share/zabbix
+#Alias /zabbix /usr/share/zabbix
+
+<Directory "/usr/share/zabbix">
+    Options FollowSymLinks
+    AllowOverride None
+    Require all granted
+
+    <IfModule mod_php5.c>
+        php_value max_execution_time 300
+        php_value memory_limit 128M
+        php_value post_max_size 16M
+        php_value upload_max_filesize 2M
+        php_value max_input_time 300
+        php_value always_populate_raw_post_data -1
+        php_value date.timezone Europe/Minsk
+    </IfModule>
+</Directory>
+
+<Directory "/usr/share/zabbix/conf">
+    Require all denied
+</Directory>
+
+<Directory "/usr/share/zabbix/app">
+    Require all denied
+</Directory>
+
+<Directory "/usr/share/zabbix/include">
+    Require all denied
+</Directory>
+
+<Directory "/usr/share/zabbix/local">
+    Require all denied
+</Directory>
+EOFZCF
+systemctl enable httpd
+systemctl restart httpd
+yum -y install zabbix-get
